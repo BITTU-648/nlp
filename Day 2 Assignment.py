d3b437b20097cd19d204bf39dd8a8ee427c0c034
BITTU-648 <doosamudijeshwanth123@gmail.com>
2025-01-29 10:52:40 +0530

    Add files via upload

---
+++ Day 2 Assignment.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_list = [10, 20, 30, 40, 50]
+
+print(my_list[0])  
+print(my_list[2])  
+print(my_list[-1])
+
+
+# In[2]:
+
+
+my_tuple = (1, 2, 3, 4, 5)
+
+print(my_tuple[0])
+print(my_tuple[2])  
+print(my_tuple[-1]) 
+
+
+# In[3]:
+
+
+my_dict = {"a": 100, "b": 200, "c": 300, "d": 400, "e": 500}
+
+print(my_dict["a"]) 
+print(my_dict["c"]) 
+print(my_dict["e"])  
+
+
+# In[ ]:
+
+
+
+
---
+++ Day 4 Assignment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n = int(input("Enter a positive integer: "))
+
+sum_even = sum(range(2, n + 1, 2))
+
+print(f"The sum of all even numbers between 1 and {n} is {sum_even}")
+
+
+# In[ ]:
+
+
+
+
